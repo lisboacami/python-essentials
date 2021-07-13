a0466a3103c0b53be02ab4496713d8ea79e0d4cd
Camila Lisboa <mila.lisboa91@hotmail.com>
2021-07-13 00:09:30 -0300

    meu primeiro commit

---
+++ Secao-4/01 - divisao.py
@@ -0,0 +1,5 @@
+# 1
+
+num = 8 / 2
+
+print(int(num))
---
+++ Secao-4/main.py
@@ -0,0 +1,29 @@
+"""
+Resolução dos exercícios da Seção 4 do Curso Geek University Programação Python Essentials
+
+"""
+
+# 1
+
+num = 8 / 2
+
+print(int(num))
+
+# 2
+
+num = 7 / 2
+
+print(num)
+
+# 3
+
+valor_1 = input('Digite um valor inteiro: ')
+valor_2 - input('Digite um valor inteiro: ')
+valor_3 - input('Digite um valor inteiro: ')
+soma = int(valor_1) + int(valor_2) + int(valor_3)
+
+print(f'A soma dos valores é: {soma}')
+
+
+
+
